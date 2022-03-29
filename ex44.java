de1c7ec073d8e5c146e09d6d9cb7c25a55094085
CHOI-YEON-CHEOL <72552897+choiyen@users.noreply.github.com>
2022-03-29 20:04:32 +0900

    Add files via upload

---
+++ ex44.java
@@ -0,0 +1,66 @@
+import java.lang.Thread.State;
+import java.util.*;
+
+import javax.naming.spi.DirStateFactory.Result;
+
+public class ex44 {
+    
+    public static boolean isOperator(char c)
+    {
+        if(c== '+' || c== '-' || c== '/' || c== '*' || c == '%')
+        {
+            return true;
+        }
+        return false;
+    }
+    public static int postfix(int a , int b, char oper)
+    {
+        if(oper == '+')
+        {
+            return a+b;
+        }
+        else if(oper == '-')
+        {
+            return a-b;
+        }
+        else if(oper == '/')
+        {
+            return a/b;
+        }
+        else if(oper == '*')
+        {
+            return a*b;
+        }
+        else
+        {
+            return a%b;
+        }
+        
+    }
+    public static void solution(String s)
+    {
+        int Result = 0;
+        Stack <Integer> stack = new Stack<>();
+        int a = 0, b = 0, z = 0;
+        for(char x : s.toCharArray())
+        {
+            if(isOperator(x))
+            {
+               b = stack.pop();
+               a = stack.pop();
+               z = postfix(a, b, x);
+               stack.push(z);                 
+            }
+            else
+            {
+                stack.push(Character.getNumericValue(x));
+            }
+        }
+        Result = stack.pop();
+        System.out.println(Result);
+    }
+    public static void main(String[] args) {
+            String str="352+*9-";
+            solution(str);
+    }
+}
